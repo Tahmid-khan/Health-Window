38d0f5a0d540a7a30eb600e059504aff6ef489e0
Md Ahnaf Tahmid Khan <ahnaf2186@gmail.com>
2025-08-12 00:08:46 +0100

    Remove old files

--- backend/routes/availabilityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const availabilityController = require('../controllers/availabilityController');
-
-router.post('/', availabilityController.createAvailability);
-router.get('/:doctorId', availabilityController.getAvailabilityByDoctor);
-
-module.exports = router;
--- backend/routes/timeslotRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateTimeSlots } = require('../controllers/timeslotController');
-
-router.post('/generate', generateTimeSlots);
-
-module.exports = router;
